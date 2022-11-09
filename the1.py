5176f721f7673ae1e8427477c2c107745deea54b
ALI KOMURCU <e2380699@ceng.metu.edu.tr>
2022-11-10 01:49:48 +0300

    Fourth commit

--- the1.py
+++ the1.py
@@ -2,34 +2,48 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import time
-def calculate_perchannel_histogram(im, nbins):
+
+hash_3d = {}
+hash_pc = {}
+
+support = []
+support_names = []
+def calculate_perchannel_histogram(im, nbins, name):
+    if hash_pc.get(name) != None:
+        return hash_pc.get(name)
     q = int(256/nbins)
-    h = np.zeros(nbins * 3)
-    h = np.reshape(h, (3, nbins))
 
-    for row in range(im.shape[0]):
-        for col in range(im.shape[1]):
-            val = im[row, col]
-            v = val//q      # int division
-            h[0][v[0]] += 1     # B
-            h[1][v[1]] += 1     # G
-            h[2][v[2]] += 1     # R
-            B = l1_normalization(h[0][v[0]])
-            G = l1_normalization(h[1][v[1]])
-            R = l1_normalization(h[2][v[2]])
-    return (B,G,R)
+    blue = np.zeros(nbins)
+    green = np.zeros(nbins)
+    red = np.zeros(nbins)
+    for i in range(nbins):
+        blue[i] = (im[:,:,0] // q == i).sum()
+        green[i] = (im[:,:,1] // q == i).sum()
+        red[i] = (im[:,:,2] // q == i).sum()
+    B = l1_normalization(blue)
+    G = l1_normalization(green)
+    R = l1_normalization(red)
+    hash_pc[name] = [B, G, R]
+    return [B,G,R]
 
 
-def calculate_3d_histogram(im, nbins):
+def calculate_3d_histogram(im, nbins, name):
+    if hash_3d.get(name) != None:
+        return hash_3d.get(name)
     q = int(256/nbins)
     h = np.zeros(pow(nbins, 3))
-    h = np.reshape(h, (nbins, nbins, nbins))
-    for row in range(im.shape[0]):
-        for col in range(im.shape[1]):
-            val = im[row, col]
-            v = val//q      # int division
-            h[v[0], v[1], v[2]] += 1
-    return h    # TODO: add l1_normalization here
+    #im = im.ravel()
+    h = h.ravel()
+    im = im // q
+
+    for i in range(nbins ** 3):
+        h[i] += (im[:, :, 0] == i).sum()
+        h[i] += (im[:, :, 1] == i).sum()
+        h[i] += (im[:, :, 2] == i).sum()
+
+    l_h = l1_normalization(h)
+    hash_3d[name] = l_h
+    return l_h    # TODO: add l1_normalization here
 
 def calculate_jsd(s, q, n):
     return 0.5 * (calculate_kl(q, (s+q)/2) + calculate_kl(s, (s+q)/2))
@@ -40,30 +54,41 @@ def calculate_kl(s, q):
 def l1_normalization(h):    # For 1D
     n = np.linalg.norm(h, 1)
     if n == 0:
-        return 0
+        n = 0.00001
     res = h/n
+
+    res[np.where(res == 0)] = 0.00001
 
     return res
 
-def query_compare(support, query, nbins, nameList, thrreD = False):
+def check_names(name1, name2):
+    if name1.split('/')[2] == name2.split('/')[2]:
+        return True
+    return False
+
+def query_compare(support, query, query_names, nbins, nameList, thrreD, gridSize):
     correct_retrieve = 0
     total = len(nameList)
+    count_q = 0
+    minS = ""
     for qname in query:
-        qIm = cv.imread(qname)
         minJSD = 99999999 # some large number TODO: look here! maybe flt_max
+        count_s = 0
+
+        if thrreD:
+            qHist = calculate_3d_histogram(qname, nbins, query_names[count_q])
+        else:
+            qHist = calculate_perchannel_histogram(qname, nbins, query_names[count_q])
         for sname in support:
-            sIm = cv.imread(sname)
             if thrreD:
-                sHist = calculate_3d_histogram(sIm, nbins)
-                qHist = calculate_3d_histogram(qIm, nbins)
+                sHist = calculate_3d_histogram(sname, nbins, support_names[count_s])
 
                 jsd = calculate_jsd(sHist, qHist, nbins)
                 if jsd < minJSD:
                     minJSD = jsd
-                    minS = sname
+                    minS = support_names[count_s]
             else:
-                sHist = calculate_perchannel_histogram(sIm, nbins)
-                qHist = calculate_perchannel_histogram(qIm, nbins)
+                sHist = calculate_perchannel_histogram(sname, nbins, support_names[count_s])
 
                 jsdB = calculate_jsd(sHist[0], qHist[0], nbins)  # B
                 jsdG = calculate_jsd(sHist[1], qHist[1], nbins)  # G
@@ -72,12 +97,24 @@ def query_compare(support, query, nbins, nameList, thrreD = False):
 
                 if jsd < minJSD:
                     minJSD = jsd
-                    minS = sname
+                    minS = support_names[count_s]
+            count_s += 1
 
-        if minS == qname:
+        if check_names(minS, query_names[count_q]):
             correct_retrieve += 1
+        count_q += 1
     top1_acc = correct_retrieve / total
     return top1_acc
+
+
+def grid(n, im):
+    height = im.shape[0]
+    grids = []
+    for i in range(n):
+        for j in range(n):
+            x = im[i*n: (i+1)*n, j*n:(j+1)*n]
+            grids.append(x)
+    return
 
 def main():
 #    im = cv.imread("dataset/support_96/Acadian_Flycatcher_0016_887710060.jpg")
@@ -86,120 +123,129 @@ def main():
     nameList = [x[:-1] for x in nameList[:-1]]   # delete \n at the end but not delete from the last one !
     file.close()
 
-    support = []
+
     # Support
     for name in nameList:
         name = "dataset/support_96/" + name
         im = cv.imread(name)
         support.append(im)
-
+        support_names.append(name)
     q_1 = []
+    q_1_names = []
     # Query 1
     for name in nameList:
         name = "dataset/query_1/" + name
         im = cv.imread(name)
         q_1.append(im)
-
+        q_1_names.append(name)
     q_2 = []
+    q_2_names = []
     # Query 2
     for name in nameList:
         name = "dataset/query_2/" + name
         im = cv.imread(name)
         q_2.append(im)
-
+        q_2_names.append(name)
     q_3 = []
+    q_3_names = []
     # Query 3
     for name in nameList:
         name = "dataset/query_3/" + name
         f = open(name, "r")
         im = cv.imread(name)
         q_3.append(im)
+        q_3_names.append(name)
+    out = open("result.txt", "w")
 
-    # Question 1
-        #Query 1
-    question_1_16 = query_compare(support, q_1, 16, nameList, True)
-    question_1_8 = query_compare(support, q_1, 8, nameList, True)
-    question_1_4 = query_compare(support, q_1, 4, nameList, True)
-    question_1_2 = query_compare(support, q_1, 2, nameList, True)
-        #Query 2
-    question_1_16 = query_compare(support, q_2, 16, nameList, True)
-    question_1_8 = query_compare(support, q_2, 8, nameList, True)
-    question_1_4 = query_compare(support, q_2, 4, nameList, True)
-    question_1_2 = query_compare(support, q_2, 2, nameList, True)
-        #Query 3
-    question_1_16 = query_compare(support, q_3, 16, nameList, True)
-    question_1_8 = query_compare(support, q_3, 8, nameList, True)
-    question_1_4 = query_compare(support, q_3, 4, nameList, True)
-    question_1_2 = query_compare(support, q_3, 2, nameList, True)
+    # # Question 1
+    #     #Query 1
+    # question_1_16 = query_compare(support, q_1, q_1_names, 16, nameList, True, 0)
+    # question_1_8 = query_compare(support, q_1, q_1_names, 8, nameList, True, 0)
+    # question_1_4 = query_compare(support, q_1, q_1_names, 4, nameList, True, 0)
+    # question_1_2 = query_compare(support, q_1, q_1_names, 2, nameList, True, 0)
+    # print("1_2" , question_1_2)
+    #     #Query 2
+    # question_1_16 = query_compare(support, q_2, q_2_names, 16, nameList, True, 0)
+    # question_1_8 = query_compare(support, q_2, q_2_names, 8, nameList, True, 0)
+    # question_1_4 = query_compare(support, q_2, q_2_names, 4, nameList, True, 0)
+    # question_1_2 = query_compare(support, q_2, q_2_names, 2, nameList, True, 0)
+    #     #Query 3
+    # question_1_16 = query_compare(support, q_3, q_3_names, 16, nameList, True, 0)
+    # question_1_8 = query_compare(support, q_3, q_3_names, 8, nameList, True, 0)
+    # question_1_4 = query_compare(support, q_3, q_3_names, 4, nameList, True, 0)
+    # question_1_2 = query_compare(support, q_3, q_3_names, 2, nameList, True, 0)
+    #
+    # # Question 2
+    #     #Query 1
+    # question_2_16 = query_compare(support, q_1, q_1_names, 16, nameList, False, 0)
+    # question_2_8 = query_compare(support, q_1, q_1_names, 8, nameList, False, 0)
+    # question_2_4 = query_compare(support, q_1, q_1_names, 4, nameList, False, 0)
+    # question_2_2 = query_compare(support, q_1, q_1_names, 2, nameList, False, 0)
+    # question_2_1 = query_compare(support, q_1, q_1_names, 8, nameList, False, 0)
+    # print("2_1", question_2_1)
+    #     #Query 2
+    # question_2_16 = query_compare(support, q_2, q_2_names, 16, nameList, False, 0)
+    # question_2_8 = query_compare(support, q_2, q_2_names, 8, nameList, False, 0)
+    # question_2_4 = query_compare(support, q_2, q_2_names, 4, nameList, False, 0)
+    # question_2_2 = query_compare(support, q_2, q_2_names, 2, nameList, False, 0)
+    # question_2_1 = query_compare(support, q_2, q_2_names, 1, nameList, False, 0)
+    #     #Query 3
+    # question_2_16 = query_compare(support, q_3, q_3_names, 16, nameList, False, 0)
+    # question_2_8 = query_compare(support, q_3, q_3_names, 8, nameList, False, 0)
+    # question_2_4 = query_compare(support, q_3, q_3_names, 4, nameList, False, 0)
+    # question_2_2 = query_compare(support, q_3, q_3_names, 2, nameList, False, 0)
+    # question_2_1 = query_compare(support, q_3, q_3_names, 1, nameList, False, 0)
+    #
+    # # Question 3
+    #     #2x2
+    # question_3_1_3D = query_compare(support, q_1, q_1_names, 16, nameList, True, 2)
+    question_3_1_pc = query_compare(support, q_1, q_1_names, 16, nameList, False, 2)
+    #     #4x4
+    # question_3_2_3D = query_compare(support, q_1, q_1_names, 16, nameList, True, 4)
+    # question_3_2_pc = query_compare(support, q_1, q_1_names, 16, nameList, False, 4)
+    #     #6x6
+    # question_3_3_3D = query_compare(support, q_1, q_1_names, 16, nameList, True, 8)
+    # question_3_3_pc = query_compare(support, q_1, q_1_names, 16, nameList, False, 8)
+    #     #8x8
+    # question_3_4_3D = query_compare(support, q_1, q_1_names, 16, nameList, True, 16)
+    # question_3_4_pc = query_compare(support, q_1, q_1_names, 16, nameList, False, 16)
+    #
+    # # Question 4
+    #     #2x2
+    # question_4_1_3D = query_compare(support, q_2, q_2_names, 16, nameList, True, 2)
+    # question_4_1_pc = query_compare(support, q_2, q_2_names, 16, nameList, False, 2)
+    #     #4x4
+    # question_4_2_3D = query_compare(support, q_2, q_2_names, 16, nameList, True, 4)
+    # question_4_2_pc = query_compare(support, q_2, q_2_names, 16, nameList, False, 4)
+    #     #6x6
+    # question_4_3_3D = query_compare(support, q_2, q_2_names, 16, nameList, True, 8)
+    # question_4_3_pc = query_compare(support, q_2, q_2_names, 16, nameList, False, 8)
+    #     #8x8
+    # question_4_4_3D = query_compare(support, q_2, q_2_names, 16, nameList, True, 16)
+    # question_4_4_pc = query_compare(support, q_2, q_2_names, 16, nameList, False, 16)
+    #
+    # # Question 5
+    #     #2x2
+    # question_5_1_3D = query_compare(support, q_3, q_3_names, 16, nameList, True, 2)
+    # question_5_1_pc = query_compare(support, q_3, q_3_names, 16, nameList, False, 2)
+    #     #4x4
+    # question_5_2_3D = query_compare(support, q_3, q_3_names, 16, nameList, True, 4)
+    # question_5_2_pc = query_compare(support, q_3, q_3_names, 16, nameList, False, 4)
+    #     #6x6
+    # question_5_3_3D = query_compare(support, q_3, q_3_names, 16, nameList, True, 8)
+    # question_5_3_pc = query_compare(support, q_3, q_3_names, 16, nameList, False, 8)
+    #     #8x8
+    # question_5_4_3D = query_compare(support, q_3, q_3_names, 16, nameList, True, 16)
+    # question_5_4_pc = query_compare(support, q_3, q_3_names, 16, nameList, False, 16)
 
-    # Question 2
-        #Query 1
-    question_2_16 = query_compare(support, q_1, 16, nameList, False)
-    question_2_8 = query_compare(support, q_1, 8, nameList, False)
-    question_2_4 = query_compare(support, q_1, 4, nameList, False)
-    question_2_2 = query_compare(support, q_1, 2, nameList, False)
-    question_2_1 = query_compare(support, q_1, 2, nameList, False)
-        #Query 2
-    question_2_16 = query_compare(support, q_2, 16, nameList, False)
-    question_2_8 = query_compare(support, q_2, 8, nameList, False)
-    question_2_4 = query_compare(support, q_2, 4, nameList, False)
-    question_2_2 = query_compare(support, q_2, 2, nameList, False)
-    question_2_1 = query_compare(support, q_2, 2, nameList, False)
-        #Query 3
-    question_2_16 = query_compare(support, q_3, 16, nameList, False)
-    question_2_8 = query_compare(support, q_3, 8, nameList, False)
-    question_2_4 = query_compare(support, q_3, 4, nameList, False)
-    question_2_2 = query_compare(support, q_3, 2, nameList, False)
-    question_2_1 = query_compare(support, q_3, 2, nameList, False)
 
-    # Question 3
-        #2x2
-    question_3_1_3D = query_compare(support, q_1, 16, nameList, True)
-    question_3_1_pc = query_compare(support, q_1, 16, nameList, False)
-        #4x4
-    question_3_2_3D = query_compare(support, q_1, 16, nameList, True)
-    question_3_2_pc = query_compare(support, q_1, 16, nameList, False)
-        #6x6
-    question_3_3_3D = query_compare(support, q_1, 16, nameList, True)
-    question_3_3_pc = query_compare(support, q_1, 16, nameList, False)
-        #8x8
-    question_3_4_3D = query_compare(support, q_1, 16, nameList, True)
-    question_3_4_pc = query_compare(support, q_1, 16, nameList, False)
-
-    # Question 4
-        #2x2
-    question_4_1_3D = query_compare(support, q_2, 16, nameList, True)
-    question_4_1_pc = query_compare(support, q_2, 16, nameList, False)
-        #4x4
-    question_4_2_3D = query_compare(support, q_2, 16, nameList, True)
-    question_4_2_pc = query_compare(support, q_2, 16, nameList, False)
-        #6x6
-    question_4_3_3D = query_compare(support, q_2, 16, nameList, True)
-    question_4_3_pc = query_compare(support, q_2, 16, nameList, False)
-        #8x8
-    question_4_4_3D = query_compare(support, q_2, 16, nameList, True)
-    question_4_4_pc = query_compare(support, q_2, 16, nameList, False)
-
-    # Question 5
-        #2x2
-    question_5_1_3D = query_compare(support, q_3, 16, nameList, True)
-    question_5_1_pc = query_compare(support, q_3, 16, nameList, False)
-        #4x4
-    question_5_2_3D = query_compare(support, q_3, 16, nameList, True)
-    question_5_2_pc = query_compare(support, q_3, 16, nameList, False)
-        #6x6
-    question_5_3_3D = query_compare(support, q_3, 16, nameList, True)
-    question_5_3_pc = query_compare(support, q_3, 16, nameList, False)
-        #8x8
-    question_5_4_3D = query_compare(support, q_3, 16, nameList, True)
-    question_5_4_pc = query_compare(support, q_3, 16, nameList, False)
-
-
-im = cv.imread("dataset/support_96/Green_tailed_Towhee_0015_136678400.jpg")
-a = calculate_3d_histogram(im,2)
-b = calculate_perchannel_histogram(im, 2)
+# im = cv.imread("dataset/support_96/Green_tailed_Towhee_0015_136678400.jpg")
+# a = calculate_3d_histogram(im, 2)
+# # x = calculate_3d_histogram_2(im, 2)
+# b = calculate_perchannel_histogram(im, 2)
+# b = calculate_perchannel_histogram2(im, 2)
 # # print("a")
-# start = time.time()
-# main()
-# end = time.time()
-# print(end - start)
+start = time.time()
+main()
+end = time.time()
+print("time: " , end - start)
